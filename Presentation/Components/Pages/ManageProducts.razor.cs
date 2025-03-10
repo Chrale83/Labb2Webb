8a688d080dccc318cc36a6f280a8dd60d1e73dc6
Chrale83 <86562475+Chrale83@users.noreply.github.com>
2025-03-10 20:30:39 +0100

    create got products  from API

---
+++ Presentation/Components/Pages/ManageProducts.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Presentation.DTOs;
+
+namespace Presentation.Components.Pages
+{
+    public partial class ManageProducts
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        public List<ProductDtoApi> Products { get; set; } = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                Products =
+                    await Http.GetFromJsonAsync<List<ProductDtoApi>>("/api/products") ?? new();
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Fel vid hämtning av produktlista{ex.Message}");
+                Products = new List<ProductDtoApi>();
+            }
+        }
+    }
+}
