b01f4e50345dccf7288d7c57adc7cce0e7cf7954
Chrale83 <86562475+Chrale83@users.noreply.github.com>
2025-03-13 09:32:03 +0100

    create design for edit product done

---
+++ Presentation/Components/Pages/EditProduct.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Presentation.DTOs;
+
+namespace Presentation.Components.Pages
+{
+    public partial class EditProduct
+    {
+        [SupplyParameterFromForm]
+        public ProductFrontDto? EditingProduct { get; set; } =
+            new ProductFrontDto
+            {
+                Name = "TEST",
+                Description = "hdjkshdksjdhsjkdhskjdhsdjkhsdkjshdkjshdkjshdkjsd",
+            };
+
+        public List<CategoryDtoApi> Categories { get; set; } =
+            new()
+            {
+                new CategoryDtoApi { Id = 1, Name = "grafik" },
+                new CategoryDtoApi { Id = 2, Name = "skärm" },
+            };
+    }
+}
