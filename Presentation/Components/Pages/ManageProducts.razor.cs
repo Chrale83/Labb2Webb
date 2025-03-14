c1dfe343982a24a6726a38fcbc84335e0593b3a7
Chrale83 <86562475+Chrale83@users.noreply.github.com>
2025-03-14 15:34:40 +0100

    create feature/searchProduct

--- Presentation/Components/Pages/ManageProducts.razor.cs
+++ Presentation/Components/Pages/ManageProducts.razor.cs
@@ -16,11 +16,62 @@ namespace Presentation.Components.Pages
         public AppState appState { get; set; }
 
         private MudTable<ProductFrontDto>? mudTable;
-        private IEnumerable<ProductFrontDto> Products { get; set; } = new List<ProductFrontDto>();
+        private List<ProductFrontDto> Products { get; set; } = new List<ProductFrontDto>();
+        private List<ProductFrontDto> SearchedProducts { get; set; } = new List<ProductFrontDto>();
         public IEnumerable<CategoryDtoApi> Categories { get; set; } = new List<CategoryDtoApi>();
 
         [Parameter]
         public ProductFrontDto? SelectedProduct { get; set; }
+
+        [Parameter]
+        public string searchText { get; set; } = string.Empty;
+        private bool isSearching = false;
+
+        private void SearchProducts(ChangeEventArgs input)
+        {
+            var textString = input.Value?.ToString();
+            if (textString.Count() > 1)
+            {
+                SearchedProducts = Products
+                    .Where(p => p.Name.Contains(textString, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+                isSearching = true;
+            }
+            else
+            {
+                isSearching = false;
+            }
+        }
+
+        private void SearchProducts2(string text)
+        {
+            if (!string.IsNullOrWhiteSpace(text) && text.Length >= 2)
+            {
+                SearchedProducts = Products
+                    .Where(p => p.Name.Contains(text, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+            }
+            else
+            {
+                SearchedProducts.Clear();
+            }
+        }
+
+        //private bool resetValueOnEmptyText;
+        //private bool coerceText;
+        //private bool coerceValue;
+        //private bool selectedOnTab;
+        //private string value2;
+
+        //private async Task<IEnumerable<string>> SearchProduct(string value, CancellationToken token)
+        //{
+        //    if (string.IsNullOrWhiteSpace(value))
+        //        return new List<string>();
+
+        //    return Products
+        //        .Where(p => p.Name.Contains(value, StringComparison.InvariantCultureIgnoreCase))
+        //        .Select(p => p.Name);
+        //}
 
         protected override async Task OnInitializedAsync()
         {
