9b85d39ab46f1fc8adead4fdd453b2d723d59811
Chrale83 <86562475+Chrale83@users.noreply.github.com>
2025-03-14 12:10:58 +0100

    .

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
-builder.Services.AddScoped(sp => new HttpClient
+builder.Services.AddSingleton(sp => new HttpClient
 {
     BaseAddress = new Uri("https://localhost:7262"),
 });
